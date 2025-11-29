05f3a03555aaa41f9c21f74303d1099730d9fbb5
vuvuzela92 <kostyaostrom@gmail.com>
2025-11-29 17:11:34 +0300

    Upload projects

--- src/advert_spend/main.py
+++ src/advert_spend/main.py
@@ -1,5 +1,5 @@
 from utils_adv_spend import main_adv_spend
 
 if __name__== "__main__":
-    days_count = 31
+    days_count = 1
     main_adv_spend(days_count)
--- src/funnel/main.py
+++ src/funnel/main.py
@@ -3,6 +3,6 @@ import asyncio
 
 if __name__ == "__main__":
     # Выбираем нужное количество дней
-    days_count = 31
+    days_count = 1
     # Получаем данные
     asyncio.run(main_funnel_daily(days_count))
